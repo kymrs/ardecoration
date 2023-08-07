7be59541f52bb8480f84a9e03a2a4bdfb81d096a
Rizky Saputra <muhamadrizkysaputra34@gmail.com>
2023-08-07 18:08:25 +0700

    Mengatur responsive

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // Toggle class active
 const navbarNav = document.querySelector(".nav");
+const content = document.querySelector(".content");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
+  content.classList.toggle("active1");
 };
 
 // klik diluar side bar untuk menghilangkan nav
